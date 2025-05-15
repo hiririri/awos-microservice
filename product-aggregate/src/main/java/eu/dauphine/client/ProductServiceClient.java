2ae45be4d4fcd93c4e89152cf00fccb0d6dac4ab
Qinming JIANG <jiangqinming005@gmail.com>
2025-05-15 09:39:52 +0200

    Move registry-server config in local

--- product-aggregate/src/main/java/eu/dauphine/client/ProductServiceClient.java
+++ product-aggregate/src/main/java/eu/dauphine/client/ProductServiceClient.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "product-service",
-    url = "http://localhost:8081",
              path = "/api/v1")
 public interface ProductServiceClient {
 
